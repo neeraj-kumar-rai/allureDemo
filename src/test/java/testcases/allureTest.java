470bf8de32d77aa4e3a65a407055d0c62cfd3658
neeraj <raikumarneeraj2000@gamil.com>
2020-12-29 19:03:14 +0530

    allure

---
+++ src/test/java/testcases/allureTest.java
@@ -0,0 +1,12 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class allureTest {
+
+    @Test
+    void justAnExample() {
+       System.out.print("allure Demo project");
+    }
+
+}
